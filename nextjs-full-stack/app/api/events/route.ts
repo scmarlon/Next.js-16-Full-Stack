44b87e9d4d23f1e28ba2d7f29b0eaf4e2b69f448
scmarlon <marsc_97@hotmail.com>
2025-11-27 19:07:19 -0600

    Create full API REST Front>Back>DB>Back>Front

---
+++ nextjs-full-stack/app/api/events/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import  connectDB  from "@/lib/mongodb";
+import  Event  from "@/database/event.model";
+
+export async function POST (request: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await request.formData();
+
+    let event;
+    try{
+        event = Object.fromEntries(formData.entries());
+    } catch(e){
+        return NextResponse.json(
+            { message: "Invalid JSON data format"}, {status: 400});
+    }
+
+    const createdEvent = await Event.create(event);
+
+    return NextResponse.json(
+        { message: "Event created successfully", event: createdEvent }, {status: 201});
+
+  }
+  catch (e) {
+    console.error(e)
+    return NextResponse.json(
+        { message: "Event creation Failed", error: e instanceof Error ? e.message : 'Unknow' }, {status: 500});
+  }}
