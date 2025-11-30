5001e86810ee42f75b3bc981ff87d1741b5b4c8c
scmarlon <marsc_97@hotmail.com>
2025-11-29 20:29:56 -0600

    last v

--- nextjs-full-stack/app/page.tsx
+++ nextjs-full-stack/app/page.tsx
@@ -18,6 +18,7 @@ const Page = async () => {
     <section>
       <h1 className="text-center">The Hub For Every Dev <br/> Even You Cant´t Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups and Conferences. All in One Place!</p>
+      <p className="text-center mt-5">Hackathons, Meetups and Conferences. All in One Place!</p>
       <ExploreBtn/>
 
       <div className="mt-20 space-y-7">
