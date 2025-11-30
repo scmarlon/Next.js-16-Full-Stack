1902fbd1073a496473478cfe70741e72f3da17de
scmarlon <marsc_97@hotmail.com>
2025-11-29 21:53:35 -0600

    last plss

--- nextjs-full-stack/app/page.tsx
+++ nextjs-full-stack/app/page.tsx
@@ -2,18 +2,13 @@ import ExploreBtn from "@/components/ExploreBtn"
 import EventCard from "@/components/EventCard"
 import {IEvent} from "@/database";
 import { cacheLife } from "next/cache";
-//const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const baseUrl =
-  process.env.BASE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
   'use cache';
   cacheLife('hours')
 
-  const response = await fetch(`${baseUrl}/api/events`);
-  
-  //const response = await fetch(`${BASE_URL}/api/events`);
+  const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
 
   return (
