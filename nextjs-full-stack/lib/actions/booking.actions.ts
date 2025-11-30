9d96ba522f38827212f617a651d4bc226454294b
scmarlon <marsc_97@hotmail.com>
2025-11-29 19:18:56 -0600

    Confi the next.config befor deplo the app

--- nextjs-full-stack/lib/actions/booking.actions.ts
+++ nextjs-full-stack/lib/actions/booking.actions.ts
@@ -5,13 +5,10 @@ import Booking from '@/database/booking.model';
 import connectDB from "@/lib/mongodb";
 
 export const createBooking = async ({ eventId, slug, email }: { eventId: string; slug: string; email: string; }) => {
-    console.log('Creating booking for:', { eventId, slug, email });
     try {
         await connectDB();
-        console.log('Database connected');
 
         await Booking.create({ eventId, slug, email });
-        console.log('Booking created successfully');
 
         return { success: true};
     } catch (e) {
